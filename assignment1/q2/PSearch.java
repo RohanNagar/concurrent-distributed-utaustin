0147a008f880451f570bcee6cfd825c6fce42cf8
Rohan Nagar <rohan.nagar@utexas.edu>
2017-01-26 20:46:45 -0600

    Complete question 2, parallel search

--- assignment1/q2/PSearch.java
+++ assignment1/q2/PSearch.java
@@ -1,13 +1,62 @@
 //UT-EID=
 
-
 import java.util.*;
 import java.util.concurrent.*;
 
+public class PSearch {
 
-public class PSearch{
-  public static int parallelSearch(int k, int[] A, int numThreads){
-    // TODO: Implement your parallel search function 
-    return -1; // if not found
+  public static int parallelSearch(int k, int[] A, int numThreads) {
+    ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+    Set<Future<Integer>> futures = new HashSet<>();
+    int subarraySize = A.length / numThreads;
+    int remainder = A.length % numThreads;
+    int startIndex = 0;
+
+    // Submit tasks
+    for (int i = 0; i < numThreads; i++) {
+      int extra = i < remainder ? 1 : 0;
+
+      Callable<Integer> callable
+          = new Searcher(startIndex, k, Arrays.copyOfRange(A, startIndex, startIndex + subarraySize + extra));
+      Future<Integer> future = executorService.submit(callable);
+      futures.add(future);
+
+      startIndex += (subarraySize + extra);
+    }
+
+    // Determine if a thread found it
+    int index = -1;
+    for (Future<Integer> f : futures) {
+      try {
+        int temp = f.get();
+        if (temp != -1) index = temp;
+      } catch (InterruptedException | ExecutionException e) {
+        System.out.println("A concurrent exception occurred: " + e.getMessage());
+      }
+    }
+
+    executorService.shutdown();
+    return index; // if not found
   }
+
+  private static class Searcher implements Callable<Integer> {
+    private final int startIndex;
+    private final int x;
+    private final int[] A;
+
+    Searcher(int startIndex, int x, int[] A) {
+      this.startIndex = startIndex;
+      this.x = x;
+      this.A = A;
+    }
+
+    public Integer call() {
+      for (int i = 0; i < A.length; i++) {
+        if (A[i] == x) return startIndex + i;
+      }
+
+      return -1;
+    }
+  }
+
 }
--- assignment1/q2/SimpleTest2.java
+++ assignment1/q2/SimpleTest2.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
-public class SimpleTest {
+// TODO: Rename back to SimpleTest before submitting
+public class SimpleTest2 {
   public static void main (String[] args) {
     
     int[] A1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};

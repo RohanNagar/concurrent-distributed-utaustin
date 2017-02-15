e3a2f8efb4cb92766bdde47f6332658f1ec4b4e2
Rohan Nagar <rohan.nagar@utexas.edu>
2017-02-14 22:12:47 -0600

    Add EIDs and formatting

--- assignment2/q2/FairReadWriteLock.java
+++ assignment2/q2/FairReadWriteLock.java
@@ -1,39 +1,45 @@
+/*
+ * EIDs of group members
+ * ran679, kmg2969
+ */
 
 public class FairReadWriteLock {
-	
-	boolean inWrite = false;
-    int readCount = 0;
-    int writeCount = 0;
-    
-	public synchronized void beginRead() {
-		while(inWrite || writeCount != 0){
-			try{
-				wait();
-			} catch(InterruptedException e){}
-		}
-		readCount++;
-		
-	}
-	
-	public synchronized void endRead() {
-		readCount--;
-		notifyAll();
-	}
-	
-	public synchronized void beginWrite() {
-		writeCount++;
-		while(readCount != 0 || inWrite){
-			try{
-				wait();
-			} catch(InterruptedException e){}
-		}
-		writeCount--;
-		inWrite = true;
-	}
-	public synchronized void endWrite() {
-		inWrite = false; 
-		notifyAll();
-	}
-	
-	
+  private boolean inWrite = false;
+  private int readCount = 0;
+  private int writeCount = 0;
+
+  public synchronized void beginRead() {
+    while (inWrite || writeCount != 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("Error in beginRead.");
+      }
+    }
+    readCount++;
+
+  }
+
+  public synchronized void endRead() {
+    readCount--;
+    notifyAll();
+  }
+
+  public synchronized void beginWrite() {
+    writeCount++;
+    while (readCount != 0 || inWrite) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        System.out.println("Error in beginWrite.");
+      }
+    }
+    writeCount--;
+    inWrite = true;
+  }
+
+  public synchronized void endWrite() {
+    inWrite = false;
+    notifyAll();
+  }
 }

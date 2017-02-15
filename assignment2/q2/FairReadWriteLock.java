1cf24221abcb6d5985c6ae38ed4d2f2760ac4653
Rohan Nagar <rohan.nagar@utexas.edu>
2017-02-14 23:54:26 -0600

    Add timestamps to question 2

--- assignment2/q2/FairReadWriteLock.java
+++ assignment2/q2/FairReadWriteLock.java
@@ -3,21 +3,34 @@
  * ran679, kmg2969
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FairReadWriteLock {
   private boolean inWrite = false;
   private int readCount = 0;
   private int writeCount = 0;
 
+  private List<Long> timestamps = new ArrayList<>();
+  private List<Long> writeTimestamps = new ArrayList<>();
+
   public synchronized void beginRead() {
-    while (inWrite || writeCount != 0) {
+    // Acquire timestamp
+    Long timestamp = System.currentTimeMillis();
+    timestamps.add(timestamp);
+
+    while (inWrite || writeCount != 0
+        || (writeTimestamps.size() > 0 && writeTimestamps.get(0) < timestamp)) {
       try {
         wait();
       } catch (InterruptedException e) {
         System.out.println("Error in beginRead.");
       }
     }
-    readCount++;
 
+    // Enter read section, remove self from timestamps list
+    readCount++;
+    timestamps.remove(timestamp);
   }
 
   public synchronized void endRead() {
@@ -26,15 +39,25 @@ public class FairReadWriteLock {
   }
 
   public synchronized void beginWrite() {
+    // Acquire timestamp
+    Long timestamp = System.currentTimeMillis();
+    timestamps.add(timestamp);
+    writeTimestamps.add(timestamp);
     writeCount++;
-    while (readCount != 0 || inWrite) {
+
+    while (readCount != 0 || inWrite
+        || !timestamps.get(0).equals(timestamp)) {
       try {
         wait();
       } catch (InterruptedException e) {
         System.out.println("Error in beginWrite.");
       }
     }
+
+    // We can now enter the write - remove ourselves from the lists used for waiting conditions
     writeCount--;
+    timestamps.remove(0);
+    writeTimestamps.remove(0);
     inWrite = true;
   }
 

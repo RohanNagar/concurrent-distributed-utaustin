fd4d4c002037907c8333a68364284105a5ff8504
Kyle Grier <grier@kth.se>
2017-02-14 18:40:18 -0600

    Finished Question 2

--- assignment2/q2/FairReadWriteLock.java
+++ assignment2/q2/FairReadWriteLock.java
@@ -1,15 +1,39 @@
+
 public class FairReadWriteLock {
 	
-                        
+	boolean inWrite = false;
+    int readCount = 0;
+    int writeCount = 0;
+    
 	public synchronized void beginRead() {
+		while(inWrite || writeCount != 0){
+			try{
+				wait();
+			} catch(InterruptedException e){}
+		}
+		readCount++;
+		
 	}
 	
 	public synchronized void endRead() {
+		readCount--;
+		notifyAll();
 	}
 	
 	public synchronized void beginWrite() {
+		writeCount++;
+		while(readCount != 0 || inWrite){
+			try{
+				wait();
+			} catch(InterruptedException e){}
+		}
+		writeCount--;
+		inWrite = true;
 	}
 	public synchronized void endWrite() {
+		inWrite = false; 
+		notifyAll();
 	}
-}
 	
+	
+}

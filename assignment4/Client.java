9231635a7dafa3ba567ff99911f47d4c48a5f8a0
Kyle Grier <grier@kth.se>
2017-03-16 18:13:50 -0500

    Included files from assignment3

---
+++ assignment4/Client.java
@@ -0,0 +1,113 @@
+/*
+ * EIDs of group members
+ * ran679, kmg2969
+ */
+
+import java.util.Scanner;
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    private static final int BUFFER_SIZE = 2048;
+
+    public static void main(String[] args) {
+        DatagramSocket udpSock;
+        Socket tcpSock;
+        InetAddress ia;
+
+        String whichSock = "T";
+        if (args.length != 3) {
+            System.out.println("ERROR: Provide 3 arguments");
+            System.out.println("\t(1) <hostAddress>: the address of the server");
+            System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
+            System.out.println("\t(3) <udpPort>: the port number for UDP connection");
+            System.exit(-1);
+        }
+
+        String hostAddress = args[0];
+        int tcpPort = Integer.parseInt(args[1]);
+        int udpPort = Integer.parseInt(args[2]);
+
+        try {
+            ia = InetAddress.getByName(hostAddress);
+            udpSock = new DatagramSocket();
+            tcpSock = new Socket(ia, tcpPort);
+
+            PrintWriter pout = new PrintWriter(tcpSock.getOutputStream());
+            Scanner receiver = new Scanner(tcpSock.getInputStream());
+
+            Scanner sc = new Scanner(System.in);
+
+            while (sc.hasNextLine()) {
+                String cmd = sc.nextLine();
+                String[] tokens = cmd.split(" ");
+
+                switch (tokens[0]) {
+                    case "setmode":
+                        if (tokens[1].equals("T")) {
+                            whichSock = "T";
+                            System.out.println("TCP");
+                        } else if (tokens[1].equals("U")) {
+                            whichSock = "U";
+                            System.out.println("UDP");
+                        } else {
+                            System.out.println("Error: Please input either 'T' or 'U'");
+                        }
+
+                        break;
+
+                    case "purchase":
+                    case "cancel":
+                    case "search":
+                    case "list":
+                        if (whichSock.equals("U")) {
+                            sendAndReceiveUDP(ia, udpSock, udpPort, cmd);
+                        } else {
+                            sendAndReceiveTCP(pout, receiver, cmd);
+                        }
+
+                        break;
+
+                    default:
+                        System.out.println("ERROR: No such command");
+                }
+            }
+
+            udpSock.close();
+            tcpSock.close();
+        } catch (IOException e) {
+            System.err.println("ERROR: Unknown error occurred: " + e);
+        }
+    }
+
+    private static void sendAndReceiveUDP(InetAddress ia, DatagramSocket socket, int port, String message) {
+        byte[] toServe = message.getBytes();
+
+        try {
+            DatagramPacket sendPacket = new DatagramPacket(toServe, toServe.length, ia, port);
+            socket.send(sendPacket);
+
+            byte[] rbuff = new byte[BUFFER_SIZE];
+            DatagramPacket recPacket = new DatagramPacket(rbuff, rbuff.length);
+            socket.receive(recPacket);
+
+            String reply = new String(recPacket.getData(), 0, recPacket.getLength());
+            System.out.println(reply);
+        } catch (IOException e) {
+            System.out.println("ERROR: Unknown error occurred: " + e);
+        }
+    }
+
+    private static void sendAndReceiveTCP(PrintWriter writer, Scanner receiver, String message) {
+        writer.println(message);
+        writer.flush();
+
+        while (receiver.hasNextLine()) {
+            String reply = receiver.nextLine();
+            if (reply.equals("done")) {
+                break;
+            }
+            System.out.println(reply);
+        }
+    }
+}
---
+++ assignment4/Store.java
@@ -0,0 +1,181 @@
+/*
+ * EIDs of group members
+ * ran679, kmg2969
+ */
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Store {
+    private final Map<String, Integer> inventory;
+    private final Map<Integer, Order> orders;
+    private final Map<String, List<Order>> userOrders;
+    private final AtomicInteger orderNumber;
+
+    public Store(String inventoryFile) {
+        this.inventory = new HashMap<>();
+        this.orders = new HashMap<>();
+        this.userOrders = new HashMap<>();
+        this.orderNumber = new AtomicInteger(1);
+
+        buildInventory(inventoryFile);
+    }
+
+    /**
+     * Performs a purchase in the online store.
+     *
+     * @param username The name of the user requesting the purchase
+     * @param product The product being purchased
+     * @param quantity The amount of the product being purchased
+     * @return The message to send as a reply
+     */
+    public synchronized String purchase(String username, String product, int quantity) {
+        Integer amountInStock = inventory.get(product);
+
+        if (amountInStock == null) {
+            return "Not Available - We do not sell this product";
+        }
+
+        if (amountInStock < quantity) {
+            return "Not Available - Not enough items";
+        }
+
+        // Obtain ID
+        int id = orderNumber.getAndIncrement();
+
+        Order order = new Order(id, username, product, quantity);
+
+        inventory.replace(product, amountInStock - quantity);
+        orders.put(id, order);
+
+        if (userOrders.containsKey(username)) {
+            List<Order> orderList = userOrders.get(username);
+            orderList.add(order);
+        } else {
+            List<Order> orderList = new ArrayList<>();
+            orderList.add(order);
+
+            userOrders.put(username, orderList);
+        }
+
+
+        return "Your order has been placed, "
+                + order.getId() + " "
+                + order.getUsername() + " "
+                + order.getProduct() + " "
+                + order.getQuantity();
+    }
+
+    /**
+     * Cancels the given order.
+     *
+     * @param id The ID of the order to cancel
+     * @return The message indicating success or failure
+     */
+    public synchronized String cancel(int id) {
+        if (!orders.containsKey(id)) {
+            return id + " not found, no such order";
+        }
+
+        Order order = orders.get(id);
+        Integer amountInStock = inventory.get(order.getProduct());
+
+        inventory.replace(order.getProduct(), amountInStock + order.getQuantity());
+        orders.remove(id);
+
+        List<Order> currentOrders = userOrders.get(order.getUsername());
+        currentOrders.remove(order);
+
+        return "Order " + id + " is canceled";
+    }
+
+    /**
+     * Obtains all of the orders for a given user.
+     *
+     * @param username The user to search for
+     * @return A String listing of all of the orders for the requested user
+     */
+    public synchronized String getOrdersForUser(String username) {
+        if (!userOrders.containsKey(username)) {
+            return "No order found for " + username;
+        }
+
+        List<Order> orders = userOrders.get(username);
+        if (orders.size() == 0) {
+            return "No order found for " + username;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (Order order : orders) {
+            builder.append(order.getId());
+            builder.append(", ");
+            builder.append(order.getProduct());
+            builder.append(", ");
+            builder.append(order.getQuantity());
+            builder.append("\n");
+        }
+
+        // Remove last newline
+        builder.deleteCharAt(builder.lastIndexOf("\n"));
+
+        return builder.toString();
+    }
+
+    /**
+     * Obtains a string representation of the current state of the inventory.
+     *
+     * @return The inventory as a String
+     */
+    public synchronized String readInventory() {
+        StringBuilder builder = new StringBuilder();
+
+        // Build string with all products
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            builder.append(entry.getKey());
+            builder.append(" ");
+            builder.append(entry.getValue());
+            builder.append("\n");
+        }
+
+        // Remove last newline
+        builder.deleteCharAt(builder.lastIndexOf("\n"));
+
+        return builder.toString();
+    }
+
+    /**
+     * Initializes an inventory map given a file to read from.
+     *
+     * @param filename The file with which to populate the inventory
+     */
+    private void buildInventory(String filename) {
+        Scanner sc;
+        try {
+            sc = new Scanner(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("FATAL ERROR: Inventory file not found.");
+
+            System.exit(-1);
+            return;
+        }
+
+        // Parse the inventory file
+        while (sc.hasNextLine()) {
+            String product = sc.nextLine();
+
+            String[] tokens = product.split(" ");
+            if (tokens.length != 2) {
+                continue;
+            }
+
+            inventory.put(tokens[0], Integer.parseInt(tokens[1]));
+        }
+    }
+}

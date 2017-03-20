ff2bd3042fc7e955b4699322505b22a7834d5cad
Kyle Grier <grier@kth.se>
2017-03-19 23:57:24 -0500

    Attributes of the Server class altered Methods for Lamport's algorithm closer to being finished Added LamportClock and Timestamp from the class Github

--- assignment4/src/Server.java
+++ assignment4/src/Server.java
@@ -5,13 +5,17 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.Comparator;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 public class Server {
-    private static AtomicInteger logicClock;
+    private static Timestamp lc;
     private static int serverID;
     private static int numServers;
     private static int port;
@@ -19,7 +23,8 @@ public class Server {
     private static String address;
     private static ArrayList<String> serversAddress;
     private static ArrayList<Integer> serversPort;
-    private static LinkedList<message> processLine;
+    private static Queue<Timestamp> processLine;
+    private static boolean CSlooking;
     public static void main (String[] args) {
         if (args.length != 1) {
             System.out.println("ERROR: Please provide a CFG file");
@@ -61,11 +66,17 @@ public class Server {
         Store store = new Store(filename);
 
         // Setup Logic Clock
-        logicClock = new AtomicInteger(0);
+        lc = new Timestamp(0, serverID);
 
         // Setup queue for processes
-        processLine = new LinkedList<message>();
-
+        processLine = new PriorityQueue<Timestamp>(0,
+                new Comparator<Timestamp>() {
+                    public int compare(Timestamp a, Timestamp b) {
+                        return Timestamp.compare(a, b);
+                    }
+                });
+        //A flag to see if the server is currently attempting to get into the critical section
+        CSlooking = false;
         // Start Threads
         TCPListener tcpListener = new TCPListener(port, store);
         tcpListener.start();
@@ -92,8 +103,11 @@ public class Server {
         public void run() {
             while (true) {
                 try {
+                    if(!CSlooking && amIPregnant()){
+
+                    }
                     Socket receiveSocket = socket.accept();
-                    logicClock.getAndIncrement();
+                    lc.tick();
                     Thread worker = new Thread(new TCPWorker(store, receiveSocket));
                     worker.start();
                 } catch (IOException e) {
@@ -132,14 +146,18 @@ public class Server {
                     String[] tokens = message.split(" ");
                     if (tokens[0] == "RQT"){
                         //Request message is received
-
+                        Timestamp newEntry;
+                        int theclock = Integer.parseInt(tokens[1]);
+                        int thepid = Integer.parseInt(tokens[2]);
+                        newEntry = new Timestamp(theclock, thepid);
+                        processLine.add(newEntry);
                     }else if(tokens[0] == "REL"){
                         //Release message is received
 
                     }else{
 
                         // Perform task for client
-                        Thread requester = new Thread(new TCPRequest());
+                        //Thread requester = new Thread(new TCPRequest());
                         while(ack != numServers-1){
                             wait();
                         }
@@ -153,7 +171,7 @@ public class Server {
                         outStream.println(reply);
                         outStream.flush();
                     }
-                    logicClock.getAndIncrement();
+                    lc.tick();
                 }
             } catch (IOException e) {
                 System.out.println("ABORTING..." + e);
@@ -164,27 +182,41 @@ public class Server {
     }
 
     private static class TCPRequest implements Runnable{
+        InetAddress ia;
+        int theport;
+        TCPRequest(String aAddress, int theport){
+            try{
+                this.ia = InetAddress.getByName(aAddress);
+                this.theport = theport;
+            } catch(IOException e){
+                System.out.println("ABORTING..." + e);
+            }
+
+        }
         public void run(){
             try{
+                /*
                 for( int i = 0; i < numServers; i++){
                     if((i+1) == serverID ){
                         continue;
                     }
-                    InetAddress ia = InetAddress.getByName(serversAddress.get(i));
-                    Socket sock = new Socket(ia, serversPort.get(i));
-                    PrintWriter pout = new PrintWriter(sock.getOutputStream());
-                    Scanner receiver = new Scanner(sock.getInputStream());
-                    pout.println("RQT "+ logicClock.toString() + " " + Integer.toString(port));
-                    pout.flush();
-                    while(receiver.hasNextLine()){
-                        String reply = receiver.nextLine();
-                        if(reply == "ACK"){
-                            ack++;
-                            break;
-                        }
+                */
+                    //InetAddress ia = InetAddress.getByName(serversAddress.get(i));
+                    //Socket sock = new Socket(ia, serversPort.get(i));
+                Socket sock = new Socket(ia, theport);
+                PrintWriter pout = new PrintWriter(sock.getOutputStream());
+                Scanner receiver = new Scanner(sock.getInputStream());
+                Integer thetime = lc.getLogicalClock();
+                pout.println("RQT "+ thetime.toString() + " " + Integer.toString(port));
+                pout.flush();
+                while(receiver.hasNextLine()){
+                    String reply = receiver.nextLine();
+                    if(reply == "ACK"){
+                        ack++;
+                        break;
                     }
-                    sock.close();
                 }
+                sock.close();
             } catch(IOException e){
                 System.out.println("ABORTING..." + e);
             }
@@ -213,19 +245,25 @@ public class Server {
         }
 
     }
-    private static class message{
-        private int timestamp;
-        private int server;
+    //test to see if the front of the queue is from this server
+    private static boolean amIPregnant(){
+        Timestamp thebaby = processLine.peek();
+        if (thebaby.getPid() == serverID){
+            //she got you for 18 years
+            return true;
+        }else{
+            // aint your baby
+            return false;
+        }
+    }
 
-        message(int timestamp, int server){
-            this.timestamp = timestamp;
-            this.server = server;
-        }
-        public int gettimestamp(){
-            return this.timestamp;
-        }
-        public int getserver(){
-            return this.server;
+    private static void sendInvitesToBabyShower(){
+        for( int i = 0; i < numServers; i++) {
+            if ((i + 1) == serverID) {
+                continue;
+            }
+            TCPRequest yourMom = new TCPRequest(serversAddress.get(i), serversPort.get(i));
+            yourMom.run();
         }
     }
 }

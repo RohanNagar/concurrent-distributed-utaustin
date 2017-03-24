4adb7f037e76670b2c47204e03efc1b9ad1ace37
Rohan Nagar <rohan.nagar@utexas.edu>
2017-03-23 19:52:31 -0500

    Add String methods to Store and Order

--- assignment4/src/Order.java
+++ assignment4/src/Order.java
@@ -3,32 +3,49 @@
  * ran679, kmg2969
  */
 
-public class Order {
-    private final int id;
-    private final String username;
-    private final String product;
-    private final int quantity;
+import java.io.Serializable;
 
-    public Order(int id, String username, String product, int quantity) {
-        this.id = id;
-        this.username = username;
-        this.product = product;
-        this.quantity = quantity;
-    }
+public class Order implements Serializable {
+  private final int id;
+  private final String username;
+  private final String product;
+  private final int quantity;
 
-    public int getId() {
-        return id;
-    }
+  public Order(int id, String username, String product, int quantity) {
+    this.id = id;
+    this.username = username;
+    this.product = product;
+    this.quantity = quantity;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public String getProduct() {
-        return product;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public int getQuantity() {
-        return quantity;
-    }
+  public String getProduct() {
+    return product;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public String toString() {
+    return id + "!" + username + "!" + product + "!" + quantity;
+  }
+
+  public static Order fromString(String s) {
+    String[] tokens = s.split("!");
+
+    Integer id = Integer.parseInt(tokens[0]);
+    String username = tokens[1];
+    String product = tokens[2];
+    Integer quantity = Integer.parseInt(tokens[3]);
+
+    return new Order(id, username, product, quantity);
+  }
 }
